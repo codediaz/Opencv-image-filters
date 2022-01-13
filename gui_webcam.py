31e255cfc17441086430aa5556b9497c9bbda42a
Sergio Díaz <codediaz@outlook.es>
2022-01-12 21:12:30 -0500

    Grid in the row for lblVideo

--- gui_webcam.py
+++ gui_webcam.py
@@ -11,5 +11,9 @@ btnIniciar.grid(column=0,row=0,padx=5, pady=5)
 
 btnFinalizar = Button(root,text="Finalizar", width=45)
 btnFinalizar.grid(column=1,row=0,padx=5, pady=5)
+
+
+lblVideo = Label(root)
+lblVideo.grid(column=0,row=1,columnspan=2)
 root.mainloop()
 
