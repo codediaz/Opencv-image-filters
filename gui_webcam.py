eac05067f5ae8382eeb171b3d1b5a9e92df421f9
Sergio Díaz <codediaz@outlook.es>
2022-01-12 21:07:28 -0500

    Grid layout btnIniciar and btnFinalizar

--- gui_webcam.py
+++ gui_webcam.py
@@ -3,3 +3,13 @@ from tkinter import filedialog
 from PIL import Image
 import cv2
 import imutils
+
+root = Tk()
+
+btnIniciar = Button(root,text="Iniciar")
+btnIniciar.grid(column=0,row=0,padx=5, pady=5)
+
+btnFinalizar = Button(root,text="Finalizar")
+btnFinalizar.grid(column=1,row=0,padx=5, pady=5)
+root.mainloop()
+
