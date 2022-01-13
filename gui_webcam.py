09c65698806eb84da76094ba7893a0ad1689f469
Sergio Díaz <codediaz@outlook.es>
2022-01-12 21:10:47 -0500

    Size the buttons changed with width = 45

--- gui_webcam.py
+++ gui_webcam.py
@@ -6,10 +6,10 @@ import imutils
 
 root = Tk()
 
-btnIniciar = Button(root,text="Iniciar")
+btnIniciar = Button(root,text="Iniciar", width=45)
 btnIniciar.grid(column=0,row=0,padx=5, pady=5)
 
-btnFinalizar = Button(root,text="Finalizar")
+btnFinalizar = Button(root,text="Finalizar", width=45)
 btnFinalizar.grid(column=1,row=0,padx=5, pady=5)
 root.mainloop()
 
