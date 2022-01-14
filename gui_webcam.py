27b665b81889b1c204ae5c3ff72d5590e537e184
Sergio Díaz <codediaz@outlook.es>
2022-01-13 20:46:48 -0500

    Fixed some bugs

--- gui_webcam.py
+++ gui_webcam.py
@@ -12,7 +12,7 @@ def show():
         ret,frame = cap.read()
         if ret == True:
             frame = imutils.resize(frame, width = 640)
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2LUV)
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
             im = Image.fromarray(frame)
             img = ImageTk.PhotoImage(image =im)
             
@@ -23,6 +23,7 @@ def show():
         else:
             lblVideo.image = ""
             cap.relase()
+
         
 
 def iniciar():
@@ -30,6 +31,9 @@ def iniciar():
     print("[INFO] starting video stream...")    
     global cap
     cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+    """frames_per_seconds = 20
+    save_path='saved-media/filter.mp4'
+    config = CFEVideoConf(cap, filepath=save_path, res='480p')"""
     show()
 
 def finalizar():
@@ -104,21 +108,23 @@ cap = None
 
 root = Tk()
 
+
+
 btnIniciar = Button(root,text="Iniciar", width=45, command = iniciar)
-btnIniciar.grid(column=0,row=0,padx=5, pady=5)
+btnIniciar.grid(column=0,row=0,padx=5, pady=40)
 
 btnFinalizar = Button(root,text="Finalizar", width=45, command=finalizar)
-btnFinalizar.grid(column=1,row=0,padx=5, pady=5)
+btnFinalizar.grid(column=1,row=0,padx=5, pady=40)
 
 #Botones para seleccionar los filtros 
 btnFiltro1 = Button(root,text="Filtro1", width=25, command=Filtro1)
-btnFiltro1.grid(column=0,row=2,padx=5, pady=5)
+btnFiltro1.grid(column=0,row=2,padx=5, pady=40)
 
 btnFiltro2 = Button(root,text="Filtro2", width=25, command=Filtro2)
-btnFiltro2.grid(column=0,row=3,padx=5, pady=5)
+btnFiltro2.grid(column=0,row=3,padx=5, pady=40)
 
 btnFiltro3 = Button(root,text="Filtro3", width=25, command=Filtro3)
-btnFiltro3.grid(column=0,row=4,padx=5, pady=5)
+btnFiltro3.grid(column=0,row=4,padx=5, pady=40)
 
 #VideoStream de salida
 lblVideo = Label(root)
